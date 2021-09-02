464f05608fe680a6a3ec91a65d97a80ab3f0cb8a
Andres Benitez <abolnaits@hotmail.com>
2021-09-02 09:39:17 -0500

    Model
    Books

--- my_site/front/models.py
+++ my_site/front/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Publisher(models.Model):
+    name = models.CharField(max_length=45)
+    address = models.CharField(max_length=45)
+    city = models.CharField(max_length=45)
+    state_province = models.CharField(max_length=45)
+    country = models.CharField(max_length=45)
+    website = models.URLField()
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    email = models.EmailField()
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=45)
+    authors = models.ManyToManyField(Author)
+    publisher = models.ForeignKey(Publisher,on_delete=models.CASCADE)
+    publication_date = models.DateField()
